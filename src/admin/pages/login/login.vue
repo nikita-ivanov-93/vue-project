b0e409e68f8d2fdc85acd1ba83762b76e2cf866b
Nikita Ivanov <nikitaivanov93@mail.ru>
2021-06-18 23:10:07 +0300

    make authentication

--- src/admin/pages/login/login.vue
+++ src/admin/pages/login/login.vue
@@ -19,19 +19,19 @@
             :errorMessage="validation.firstError('user.password')"
           )
         .btn
-          app-button(title="Отправить")
+          app-button(title="Отправить" :disbled="isSubmitDisabled")
 </template>
 
 <script>
 import appInput from "../../components/input";
 import button from '../../components/button';
 import { Validator, mixin as ValidatorMixin } from "simple-vue-validator";
-
+import $axios from '../../request';
 export default {
   mixins: [ValidatorMixin],
   validators: {
     "user.name": (value) => {
-      return Validator.value(value).required("Введите имя пользователя").email("Введите коректный email");
+      return Validator.value(value).required("Введите имя пользователя");
     },
     "user.password": (value) => {
       return Validator.value(value).required("Введите пароль");
@@ -45,13 +45,24 @@ export default {
     user: {
         name: "",
         password: ""
-    }
+    },
+    isSubmitDisabled: false,
   }),
   methods: {
-    handlesubmit() {
-      this.$validate().then((isValid) => {
+    async handlesubmit() {
+      this.$validate().then(async (isValid) => {
         if (isValid === false) return;
-
+        this.isSubmitDisabled = true;
+        try {
+          const response = await $axios.post("/login", this.user);
+          const token = response.data.token;
+          localStorage.setItem("token", token);
+          $axios.defaults.headers["Authorization"] = `Bearer ${token}`;
+          this.$router.replace('/');
+        } catch (error) {console.log(error.response.data.error)}
+          finally {
+          this.isSubmitDisabled = false;
+        }
         
       })
     }
